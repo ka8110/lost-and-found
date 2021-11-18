74aaa5fbfb01c1b382dbcea5e8a45b44fadb005d
ka8110 <61128965+ka8110@users.noreply.github.com>
2021-11-18 20:38:11 +1300

    Update views.py
    Added the ability to search by getting the users request and filtering the length and if it contains anything in the database.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,15 +4,32 @@ from .models import Lostitems
 
 # Create your views here.
 
-def index(request):
+def base(request):
     
     all_items = Lostitems.objects.all().order_by('-date')
+    
     context = {"all_items":all_items}
+    
     return render(request, 'main/base.html', context)
 
 def home(request, item_id):
     
-    item = get_object_or_404(Lostitems, pk=item_id)
-    context = {'item':item}
+    items = get_object_or_404(Lostitems, pk=item_id)
+    
+    context = {'items':items}
 
     return render(request, 'main/home.html', context)
+
+def search(request):
+    
+    all_items = Lostitems.objects.all().order_by('-date')
+
+    if len(request.GET) == 0:
+        relevant_items = all_items
+    else:
+        search_string = request.GET['q']
+        relevant_items = all_items.filter(name__contains = search_string)
+    
+    context = {'relevant_items':relevant_items}
+
+    return render(request, 'main/search.html', context)
